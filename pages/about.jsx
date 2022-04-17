edf2f0d2cd9da7edf85295d240e7155c6f2e630b
Michaelmw17 <michaelmw17@outlook.com>
2022-04-17 11:08:29 +1000

    Navbar errors fixed

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+function About() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-center py-20">About</h1>
+    </>
+  );
+}
+
+export default About;
